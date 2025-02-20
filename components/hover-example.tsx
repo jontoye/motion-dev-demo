f29a1b2f9ceb3c577201c3a5ec3e15157790a0f1
jontoye <jon.toye@xanadu.ai>
2025-02-19 23:00:45 -0500

    code block formatting fix

--- components/hover-example.tsx
+++ components/hover-example.tsx
@@ -31,53 +31,53 @@ export function HoverExample() {
       >
         {`
 const MenuItem = ({ label, subLinks = [] }: MenuItemProps) => {
+  const [subMenuState, setSubMenuState] = useState<'visible' | 'hidden'>(
+    'hidden'
+  )
 
-const [subMenuState, setSubMenuState] = useState<'visible' | 'hidden'>('hidden')
+  const subMenuVariants: Variants = {
+    visible: {
+      opacity: 1,
+      y: 0,
+      display: 'block',
+      transition: {
+        damping: 0,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      y: 20,
+      transitionEnd: {
+        display: 'none',
+      },
+    },
+  }
 
-const subMenuVariants: Variants = {
-visible: {
-opacity: 1,
-y: 0,
-display: 'block',
-transition: {
-damping: 0,
-},
-},
-hidden: {
-opacity: 0,
-y: 20,
-transitionEnd: {
-display: 'none',
-},
-},
+  return (
+    <motion.li
+      className="py-4"
+      onHoverStart={() => setSubMenuState('visible')}
+      onHoverEnd={() => setSubMenuState('hidden')}
+    >
+      <span className="px-4 cursor-pointer text-button-primary">{label}</span>
+      <motion.ul
+        className="border-slate-400/25 border-2 w-36 absolute top-14 bg-button-primary"
+        animate={subMenuState}
+        variants={subMenuVariants}
+        initial="hidden"
+      >
+        {subLinks.map((subLink) => (
+          <li
+            key={subLink.label}
+            className="py-2 hover:opacity-70 transition-opacity"
+          >
+            <Link href={subLink.href}>{subLink.label}</Link>
+          </li>
+        ))}
+      </motion.ul>
+    </motion.li>
+  )
 }
-
-return (
-<motion.li 
-className="py-4"
-onHoverStart={() => setSubMenuState('visible')}
-onHoverEnd={() => setSubMenuState('hidden')}
->
-<motion.ul
-className="border-slate-400/25 border-2 w-36 absolute top-14"
-animate={subMenuState}
-variants={subMenuVariants}
-initial="hidden"
->
-{subLinks.map((subLink) => (
-  <li
-    key={subLink.label}
-    className="py-2 hover:opacity-70 transition-opacity"
-  >
-    <Link href={subLink.href}>{subLink.label}</Link>
-  </li>
-))}
-</motion.ul>
-</motion.li>
-)
-}
-
-
 `}
       </SyntaxHighlighter>
     </>
